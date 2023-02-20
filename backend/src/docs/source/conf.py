c6988b3694a330ce13ea86ac836b1ea00dd7f979
kms1212 <kms1212g@gmail.com>
2023-02-21 00:58:00 +0900

    Update conf.py

--- backend/src/docs/source/conf.py
+++ backend/src/docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsis.settings_test')
 
 import django
 django.setup()
